eb4b4c366abbb6cb33965432f067d65111f8507c
Nadav Lahish <d_lahish@MacBook-Air-2.local>
2016-08-15 17:27:44 +0200

    add dbinit to package.json

--- api/server.js
+++ api/server.js
@@ -1,8 +1,9 @@
 const express    = require('express');
 const app        = express();
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const Person = require('./models/person')
+const mongoose   = require('mongoose');
+const Person = require('./models/person');
+mongoose.connect('mongodb://localhost/klarna');
 
 const router = express.Router();
 
@@ -11,10 +12,8 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 
-mongoose.connect('mongodb://localhost/klarna');
-
 router.use(function(req, res, next) {
-    console.log('Something is happening.');
+    
     next();
 });
 
